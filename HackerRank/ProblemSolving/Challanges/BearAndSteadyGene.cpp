7a8df4c15c930ea78a95a4614fc13e5b32b0d84d
Marco Stranner <marco.stranner@outlook.com>
2023-08-02 11:38:12 +0200

    add challange BearAndSteadyGene

---
+++ HackerRank/ProblemSolving/Challanges/BearAndSteadyGene.cpp
@@ -0,0 +1,79 @@
+//https://www.hackerrank.com/challenges/bear-and-steady-gene/problem?isFullScreen=true
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <valarray> 
+#include <queue>
+
+using namespace std;
+
+string& ltrim(string &);
+string& rtrim(string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'steadyGene' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING gene as parameter.
+ */
+
+int steadyGene(string gene) {
+    int min_sub = 0;
+
+    return min_sub;
+}
+
+int main()
+{
+    //ofstream fout(getenv("OUTPUT_PATH"));
+    ofstream fout("out.txt");   
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string gene;
+    getline(cin, gene);
+
+    int result = steadyGene(gene);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+
+string &ltrim(string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int c) {return !isspace(c);}));
+    return s;
+}
+
+string &rtrim(string &s) {
+    s.erase((++s.rbegin()).base(), std::find_if((++s.rbegin()).base(), s.rbegin().base(), [](int c) {return !isspace(c);}));
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
