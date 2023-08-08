d8d3f5f95439fbe1fd2e33262a4aefe2644b7876
Marco Stranner <marco.stranner@outlook.com>
2023-08-08 09:25:12 +0200

    create 9

---
+++ HackerRank/Challanges/ProjectEuler+/09SpecialPythagoreanTriplet.cpp
@@ -0,0 +1,68 @@
+//https://www.hackerrank.com/contests/projecteuler/challenges/euler009/problem?isFullScreen=true
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <valarray> 
+#include <queue>
+
+using namespace std;
+
+string& ltrim(string &);
+string& rtrim(string &);
+vector<string> split(const string &);
+
+
+string &ltrim(string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int c) {return !isspace(c);}));
+    return s;
+}
+
+string &rtrim(string &s) {
+    s.erase((++s.rbegin()).base(), std::find_if((++s.rbegin()).base(), s.rbegin().base(), [](int c) {return !isspace(c);}));
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+int main()
+{
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        int n = stoi(ltrim(rtrim(n_temp)));
+
+        //Pythagorean triplet:
+        // a < b < c where:
+        // a^2 + b^2 = c^2
+        // Given N, check if Triple with a + b + c = N
+
+        
+    }
+
+    return 0;
+}
