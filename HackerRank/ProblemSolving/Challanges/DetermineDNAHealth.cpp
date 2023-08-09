d3e7d7412d0a28f789a8270de3769f47ed4a9503
Marco Stranner <marco.stranner@outlook.com>
2023-08-09 09:40:14 +0200

    startet dna health thing

---
+++ HackerRank/ProblemSolving/Challanges/DetermineDNAHealth.cpp
@@ -0,0 +1,110 @@
+//https://www.hackerrank.com/challenges/determining-dna-health/problem?isFullScreen=true
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <valarray> 
+#include <queue>
+
+using namespace std;
+
+string& ltrim(string &);
+string& rtrim(string &);
+vector<string> split(const string &);
+
+
+string &ltrim(string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int c) {return !isspace(c);}));
+    return s;
+}
+
+string &rtrim(string &s) {
+    s.erase((++s.rbegin()).base(), std::find_if((++s.rbegin()).base(), s.rbegin().base(), [](int c) {return !isspace(c);}));
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string genes_temp_temp;
+    getline(cin, genes_temp_temp);
+
+    vector<string> genes_temp = split(rtrim(genes_temp_temp));
+
+    vector<string> genes(n);
+
+    for (int i = 0; i < n; i++) {
+        string genes_item = genes_temp[i];
+
+        genes[i] = genes_item;
+    }
+
+    string health_temp_temp;
+    getline(cin, health_temp_temp);
+
+    vector<string> health_temp = split(rtrim(health_temp_temp));
+
+    vector<int> health(n);
+
+    for (int i = 0; i < n; i++) {
+        int health_item = stoi(health_temp[i]);
+
+        health[i] = health_item;
+    }
+
+    string s_temp;
+    getline(cin, s_temp);
+
+    int s = stoi(ltrim(rtrim(s_temp)));
+
+    for (int s_itr = 0; s_itr < s; s_itr++) {
+        string first_multiple_input_temp;
+        getline(cin, first_multiple_input_temp);
+
+        vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+        int first = stoi(first_multiple_input[0]);
+
+        int last = stoi(first_multiple_input[1]);
+
+        string d = first_multiple_input[2];
+
+
+        //gene = [g0,g1,..gn]   genes
+        //health = [h0,h1,..hn] health of gi    health
+        //sets of dna with start,end and d with healthy dna from gstart to gend
+        //print the one with min and max health
+
+        // Solve with aho-corasick algo
+
+        
+
+
+    }
+
+    return 0;
+}
